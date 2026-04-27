66c40fdbcbc0ce3d1f89850b0cd5cd5e27cd0f5c
IZxMD <56047792+IZxMD@users.noreply.github.com>
2026-04-27 13:36:48 +0200

    Add ExportStats route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { RootLayout } from './layout/RootLayout';
 import { HomePage } from './pages/HomePage';
 import { ArchaeologyPage } from '../features/archaeology/pages/ArchaeologyPage';
+import { ExportStatsPage } from '../features/exportStats/pages/ExportStatsPage';
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: 'export-stats', element: <ExportStatsPage /> },
       { path: 'archaeology', element: <ArchaeologyPage /> },
     ],
   },
